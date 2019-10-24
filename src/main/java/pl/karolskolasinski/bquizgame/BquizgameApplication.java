4e1f5f931b19356ddfe4aae1cd599538b48de7f1
Karol Skolasiński <karolskolasinski@gmail.com>
2019-10-24 18:30:07 +0200

    initial commit

---
+++ src/main/java/pl/karolskolasinski/bquizgame/BquizgameApplication.java
@@ -0,0 +1,13 @@
+package pl.karolskolasinski.bquizgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BquizgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BquizgameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/karolskolasinski/bquizgame/BquizgameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karolskolasinski.bquizgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BquizgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
