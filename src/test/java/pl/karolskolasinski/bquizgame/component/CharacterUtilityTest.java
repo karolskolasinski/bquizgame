1799ad8d36dd222267c3da787a6c5dff0a3d18f1
Karol Skolasiński <karolskolasinski@gmail.com>
2019-12-16 19:59:08 +0100

    CharsetUtilityTest

--- src/test/java/pl/karolskolasinski/bquizgame/component/CharacterUtilityTest.java
+++ src/test/java/pl/karolskolasinski/bquizgame/component/CharacterUtilityTest.java
@@ -2,14 +2,46 @@ package pl.karolskolasinski.bquizgame.component;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CharacterUtilityTest {
 
     @Test
-    void shouldReturnLetter() {
-        final int number1 = 1;
-        final int number2 = 2;
-        final int number3 = 3;
-        final int number4 = 4;
+    void shouldReturnALetterWhenNumberIs0() {
+        //given
+        CharacterUtility characterUtility = new CharacterUtility();
+        final int number0 = 0;
 
+        //then
+        assertEquals("A", characterUtility.intToCharLetter(number0));
+    }
+
+    @Test
+    void shouldReturnBLetterWhenNumberIs1() {
+        //given
+        CharacterUtility characterUtility = new CharacterUtility();
+        final int number1 = 1;
+
+        //then
+        assertEquals("B", characterUtility.intToCharLetter(number1));
+    }
+    @Test
+    void shouldReturnCLetterWhenNumberIs2() {
+        //given
+        CharacterUtility characterUtility = new CharacterUtility();
+        final int number2 = 2;
+
+        //then
+        assertEquals("C", characterUtility.intToCharLetter(number2));
+    }
+
+    @Test
+    void shouldReturnDLetterWhenNumberIs3() {
+        //given
+        CharacterUtility characterUtility = new CharacterUtility();
+        final int number3 = 3;
+
+        //then
+        assertEquals("D", characterUtility.intToCharLetter(number3));
     }
 }
