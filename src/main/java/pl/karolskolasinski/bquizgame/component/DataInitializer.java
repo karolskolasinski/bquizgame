6602adc6e273c7f02613aab7d56e9d20c5640eaa
Karol Skolasiński <karolskolasinski@gmail.com>
2020-03-05 21:33:00 +0100

    ClassLoader fixing

--- src/main/java/pl/karolskolasinski/bquizgame/component/DataInitializer.java
+++ src/main/java/pl/karolskolasinski/bquizgame/component/DataInitializer.java
@@ -8,14 +8,16 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 import pl.karolskolasinski.bquizgame.model.account.Account;
 import pl.karolskolasinski.bquizgame.model.account.AccountRole;
 import pl.karolskolasinski.bquizgame.model.schema.Answer;
 import pl.karolskolasinski.bquizgame.model.schema.Question;
 import pl.karolskolasinski.bquizgame.repository.*;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -29,7 +31,7 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
     private PasswordEncoder passwordEncoder;
     private QuestionRepository questionRepository;
     private AnswerRepository answerRepository;
-    private ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    private ClassLoader classLoader = this.getClass().getClassLoader();
 
 
     @Autowired
@@ -78,9 +80,9 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         }
     }
 
-    private void addDefaultQuestions() throws FileNotFoundException {
+    private void addDefaultQuestions() {
 //        File file = new File(Objects.requireNonNull(classLoader.getResource("questions/questions_answers.html")).getFile());
-        File file = ResourceUtils.getFile("questions/questions_answers.html");
+        File file = new File(getClass().getResource("questions/questions_answers.html").getFile());
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             tryReadFromFileAndSaveToDatabase(file, reader);
