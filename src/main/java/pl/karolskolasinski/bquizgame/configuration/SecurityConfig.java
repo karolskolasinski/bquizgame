fb170f8fa3dc9302a1753b3f8627f3a58b7c08ed
Karol Skolasiński <karolskolasinski@gmail.com>
2020-04-10 10:33:11 +0200

    cleanup

--- src/main/java/pl/karolskolasinski/bquizgame/configuration/SecurityConfig.java
+++ src/main/java/pl/karolskolasinski/bquizgame/configuration/SecurityConfig.java
@@ -24,7 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("/",
                         "/css/**",
